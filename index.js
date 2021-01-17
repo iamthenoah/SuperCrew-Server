f3325c5d48acb51f5607e6d7859112cd20da1843
Noah <greffnoah@gmail.com>
2021-01-17 01:34:58 +0100

    added cors policy

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const cors = require('cors');
+
+app.use(cors({ origin: '*' }));
 
 server.listen(process.env.PORT || 3000);
 
