96be387a21f31ab681f4868d8c30403442c9184b
Noah <greffnoah@gmail.com>
2021-01-17 02:00:47 +0100

    testing cors

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const app = require('express')();
+app.use(cors({ origin: '*' }));
+
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const cors = require('cors');
-
-app.use(cors({ origin: '*' }));
 
 server.listen(process.env.PORT || 3000);
 
@@ -11,7 +11,5 @@ io.on('connect', socket => {
 
     console.log('connected:', socket.client.id);
 
-    socket.on('serverEvent', data => console.log('new message from client:', data));
-
-    setInterval(() => socket.emit('clientEvent', Math.random()), 3000);
+    socket.on('received', (id, data) => console.log('FROM:', id, 'DATA:', data));
 });
